b4a4c733770b3965364b8738f985cb275340a4e5
Polina Chernyshova <petrpol1997@yandex.ru>
2024-05-01 18:15:50 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
 public class Main {
+    //вывод информации об объекте
+    public static void main(String[] args) {
+        Person person1 = new Person("Иванов Иван Иванович", "Инженер", "ivanov@mailbox.com", "89991231122", 33333, 35);
+        person1.printPersonInfo();
+    }
 }
-//учебный проект AQA
--- src/Park.java
+++ src/Park.java
@@ -1,3 +1,35 @@
 public class Park {
-    
+    private String name;
+    private String city;
+    private String district;
+
+    public Park(String name, String city, String district) {
+        this.name = name;
+        this.city = city;
+        this.district = district;
+    }
+
+    public class Attraction {
+        private String name;
+        private int workingHours;
+        private double cost;
+
+        public Attraction(String name, int workingHours, double cost) {
+            this.name = name;
+            this.workingHours = workingHours;
+            this.cost = cost;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getWorkingHours() {
+            return workingHours;
+        }
+
+        public double getCost() {
+            return cost;
+        }
+    }
 }
