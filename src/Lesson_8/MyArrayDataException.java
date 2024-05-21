12706b26108d5e02677258d93cf750cc3061c880
Polina Chernyshova <petrpol1997@yandex.ru>
2024-05-21 23:25:30 +0300

    code review

--- src/Lesson_8/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package Lesson_8;
-
-public class MyArrayDataException extends Exception {
-        public MyArrayDataException(int row, int column) {
-            super("Неверные данные в ячейке [" + row + "][" + column + "]");
-        }
-    }
