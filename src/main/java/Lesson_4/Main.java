b2002031a5390e1e434d185237253d3ed7b41e92
Polina Chernyshova <petrpol1997@yandex.ru>
2024-06-13 20:37:15 +0300

    Lesson_4 code review

--- src/main/java/Lesson_4/Main.java
+++ src/main/java/Lesson_4/Main.java
@@ -3,75 +3,68 @@ package Lesson_4;
 import java.util.Arrays;
 
 public class Main {
-        public static void main(String[] args) {
-            printThreeWords();
-            checkSumSign();
-            printColor();
-            compareNumbers();
-            checkSum(4, 9);
-            numPositiveOrNegative(-4);
-            isNegative(-100);
-            method8("Hello", 5);
-            method9(1999);
-            massive10();
-            massive11();
-            massive12();
-            massive13();
-            twoArgs(5, 1);
-        }
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+        checkSum(4, 9);
+        numPositiveOrNegative(-4);
+        isNegative(-100);
+        printString("Hello", 5);
+        checkYear(1999);
+        integerArray();
+        fillArray();
+        multiplyArrayElements();
+        fillArrayDiagonal();
+        createArrayByLenAndValue(5, 1);
+    }
 
-        public static void printThreeWords() {
-            System.out.println("Orange");
-            System.out.println("Banana");
-            System.out.println("Apple");
-        }
+    public static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
 
-        public static void checkSumSign() {
-            int a = 3, b = 4;
-            int sum = a + b;
-            if (sum >= 0) {
-                System.out.println("Сумма положительная");
-            } else {
-                System.out.println("Сумма отрицательная");
-            }
+    public static void checkSumSign() {
+        int a = 3, b = 4;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
         }
+    }
 
-        public static void printColor() {
-            int value = 115;
+    public static void printColor() {
+        int value = 115;
 
-            if (value <= 0) {
-                System.out.println("Красный");
-            } else if (value <= 100) {
-                System.out.println("Желтый");
-            } if (value > 100) {
-                System.out.println("Зеленый");
-            }
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
         }
+    }
 
-        public static void compareNumbers() {
-            int a = 7, b = 0;
-            if (a >= b) {
-                System.out.println("a >= b");
-            } else {
-                System.out.println("a < b");
-            }
+    public static void compareNumbers() {
+        int a = 7, b = 0;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
         }
+    }
 
-        public static boolean checkSum(int num1, int num2) {
-            int sum = num1 + num2;
-            if (sum >= 10 && sum <= 20) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
+    public static boolean checkSum(int num1, int num2) {
+        int sum = num1 + num2;
+        return (sum >= 10 && sum <= 20);
+    }
 
         public static void numPositiveOrNegative(int a) {
             if (a >= 0) {
-                System.out.println("Число" + a + "положительное");
+                System.out.println("Число " + a + " положительное");
             } else {
-                System.out.println("Число" + a + "отрицательное");
+                System.out.println("Число " + a + " отрицательное");
             }
         }
 
@@ -79,55 +72,47 @@ public class Main {
             return a < 0;
         }
 
-        public static void method8(String message, int a) {
-            for (int k = 0; k < a; k++) {
+        public static void printString(String message, int a) {
+            for (int i = 0; i < a; i++) {
                 System.out.println(message);
             }
         }
 
-        public static boolean method9(int year) {
-            if (year % 4 == 0) {
-                return true;
-            } else if (year % 100 == 0) {
-                return false;
-            } else if (year % 400 == 0) {
-                return true;
-            } else {
-                return false;
-            }
+        public static boolean checkYear(int year) {
+       return (year%4 ==0 && year%100!=0 || year/400==0);
         }
 
-        public static void massive10() {
-            int[] arr10 = {0, 1, 0, 1, 1, 0, 1, 1, 0, 0};
-            for (int i = 0; i < arr10.length; i++) {
-                if (arr10[i] == 0) {
-                    arr10[i] = 1;
+        public static void integerArray() {
+            int[] array = {0, 1, 0, 1, 1, 0, 1, 1, 0, 0};
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == 0) {
+                    array[i] = 1;
                 } else {
-                    arr10[i] = 0;
+                    array[i] = 0;
                 }
             }
-            System.out.println(Arrays.toString(arr10));
+            System.out.println(Arrays.toString(array));
         }
 
-        public static void massive11() {
-            int[] arr11 = new int[100];
+        public static void fillArray() {
+            int[] array = new int[100];
             for (int i = 0; i < 100; i++) {
-                arr11[i] = i + 1;
+                array[i] = i + 1;
             }
-            System.out.println(Arrays.toString(arr11));
+            System.out.println(Arrays.toString(array));
         }
 
-        public static void massive12() {
-            int[] arr12 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-            for (int i = 0; i < arr12.length; i++) {
-                if (arr12[i] < 6) {
-                    arr12[i] = arr12[i] * 2;
+        public static void multiplyArrayElements() {
+            int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] < 6) {
+                    array[i] = array[i] * 2;
                 }
             }
-            System.out.println(Arrays.toString(arr12));
+            System.out.println(Arrays.toString(array));
         }
 
-        public static void massive13() {
+        public static void fillArrayDiagonal() {
             int[][] array = new int[3][3];
             for (int i = 0; i < array.length; i++) {
                 for (int j = 0; j < array.length; j++) {
@@ -138,11 +123,11 @@ public class Main {
             }
         }
 
-        public static void twoArgs(int len, int initialValue) {
-            int[] arr14 = new int[len];
-            for (int i = 0; i < len; i++) {
-                arr14[i] = initialValue;
+        public static void createArrayByLenAndValue(int len, int initialValue) {
+            int[] array = new int[len];
+            for (int i=0; i < len; i++) {
+                array[i] = initialValue;
             }
-            System.out.println(Arrays.toString(arr14));
+            System.out.println(Arrays.toString(array));
         }
     }
