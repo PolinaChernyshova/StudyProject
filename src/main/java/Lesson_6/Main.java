7a19e4830750e99a7dd9f40b71ca50397602eecc
Polina Chernyshova <petrpol1997@yandex.ru>
2024-06-13 20:44:01 +0300

    Lesson_6 code review

--- src/main/java/Lesson_6/Main.java
+++ src/main/java/Lesson_6/Main.java
@@ -1,5 +1,7 @@
 package Lesson_6;
 
+import java.util.SortedMap;
+
 public class Main {
     //вывод информации об объектах
     public static void main(String[] args) {
@@ -7,7 +9,11 @@ public class Main {
         Employee firstEmployee = new Employee("Иванов Иван Иванович", "Инженер", "ivanov@mailbox.com", "89991231122", 33333, 35);
         firstEmployee.printEmployeeInfo();
         // 2
-        Park.Attraction attraction1 = new Park( "Орленок", "Воронеж", "Центральный район").new Attraction("Карусель", 9, 150);
-        System.out.println(attraction1.getAttractionName());
+        Park.Attraction firstAattraction = new Park( "Орленок", "Воронеж", "Центральный район").new Attraction("Карусель", 9, 150);
+        Park.Attraction secondAattraction = new Park( "Орленок", "Воронеж", "Центральный район").new Attraction("Ветерок", 9, 100);
+        Park.Attraction thirdAattraction = new Park( "Орленок", "Воронеж", "Центральный район").new Attraction("Американские горки", 5, 180);
+        System.out.println(firstAattraction.getAttractionName());
+        System.out.println(secondAattraction.getWorkingHours());
+        System.out.println(thirdAattraction.getCost());
     }
 }
